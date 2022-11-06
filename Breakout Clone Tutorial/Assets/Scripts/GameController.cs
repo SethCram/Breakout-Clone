1688f46533ae72860f9210b043299b6a44411557
sethhi2 <sethcram@gmail.com>
2022-11-05 22:22:12 -0600

    controller finished, large scale training possible

---
+++ Breakout Clone Tutorial/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject GameEnviron;
+
+    public float xSeperatedBy = 100;
+
+    public int trainingEnvirons = 50;
+
+    public bool training = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if training
+        if( training == true )
+        {
+            float xDistanceFromZero = xSeperatedBy;
+            //create training environs
+            for (int i = 0; i < trainingEnvirons; i++)
+            {
+                Instantiate(GameEnviron, transform.position + Vector3.right * xDistanceFromZero, Quaternion.identity);
+                xDistanceFromZero += xSeperatedBy;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
